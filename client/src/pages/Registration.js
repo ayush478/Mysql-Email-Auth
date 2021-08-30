68ac629e52ac2ba61d7a6c1592cbbeb1b66580b7
Ayush Sunariya <69513008+ayush478@users.noreply.github.com>
2021-08-30 12:44:06 +0530

    bug fixes

--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -6,10 +6,7 @@ export default function Registration() {
   const [usernameReg, setUsernameReg] = useState("");
   const [passwordReg, setPasswordReg] = useState("");
 
-  // const [username, setUsername] = useState("");
-  // const [password, setPassword] = useState("");
 
-  // const [loginStatus, setLoginStatus] = useState("");
 
   Axios.defaults.withCredentials = true;
 
@@ -22,26 +19,7 @@ export default function Registration() {
     });
   };
 
-  // const login = async () => {
-  //   await Axios.post("http://localhost:3001/login", {
-  //     username: username,
-  //     password: password,
-  //   }).then((response) => {
-  //     if (response.data.message) {
-  //       setLoginStatus(response.data.message);
-  //     } else {
-  //       setLoginStatus(response.data[0].username);
-  //     }
-  //   });
-  // };
 
-  // useEffect(() => {
-  //   Axios.get("http://localhost:3001/login").then((response) => {
-  //     if (response.data.loggedIn === true) {
-  //       setLoginStatus(response.data.user[0].username);
-  //     }
-  //   });
-  // }, []);
 
   return (
     <div className="App">
@@ -65,26 +43,7 @@ export default function Registration() {
         <button onClick={register}> Register </button>
       </div>
 
-      {/* <div className="login">
-        <h1>Login</h1>
-        <input
-          type="text"
-          placeholder="Username..."
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-        <input
-          type="password"
-          placeholder="Password..."
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <button onClick={login}> Login </button>
-      </div>
 
-      <h1>{loginStatus}</h1> */}
     </div>
   );
 }
